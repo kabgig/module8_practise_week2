b15b4c67e11f3bee5782969a1c0e42b4a531c157
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-18 21:38:49 +0400

    EX2

---
+++ src/Orc.java
@@ -0,0 +1,16 @@
+public class Orc implements Playable{
+    @Override
+    public String move() {
+        return "run";
+    }
+
+    @Override
+    public String attack() {
+        return "ax blow";
+    }
+
+    @Override
+    public String defence() {
+        return "block";
+    }
+}
---
+++ src/Playable.java
@@ -0,0 +1,5 @@
+public interface Playable {
+    String move();
+    String attack();
+    String defence();
+}
