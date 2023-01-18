76fe4d4eef90a671dbfa6aab743b93f9c0f05837
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-18 22:52:23 +0400

    EX6 still in the process

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Vector2D extends Vector{
+public class Vector2D extends Vector {
 
     public Vector2D(int x1, int y1, int x2, int y2) {
         super(x1, y1, 0, x2, y2, 0);
@@ -9,19 +9,27 @@ public class Vector2D extends Vector{
 
     @Override
     double getLength() {
-        var res = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        var res = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return Math.abs(res);
     }
 
 
     @Override
     String getVectorCords() {
-        return  x1 + " " +
+        return x1 + " " +
                 y1 + " " +
                 x2 + " " + y2;
     }
 
-    double getscalarProduct(Vector2D vector){
+    public double getscalarProduct(Vector2D vector) {
+        return (double)
+                this.x1 * vector.x1 +
+                this.x2 * vector.x2 +
+                this.y1 * vector.y1 +
+                this.y2 * vector.y2;
+    }
 
+    public double getAngle(Vector2D vector){
+        return 0.0;
     }
 }
